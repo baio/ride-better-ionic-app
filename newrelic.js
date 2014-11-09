2d27891bb6dcc72b223a075c6fe3389e4776cf3c
baio <max.putilov@gmail.com>
2014-11-09 22:03:36 +0300

    new relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['ride-better'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'cdc04e9a1643f8b59cbfe567de85e9fa2e7e39f8',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
