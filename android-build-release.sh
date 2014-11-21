6fc8730c7d1194b0de1adae3bc482bb3093874da
baio <max.putilov@gmail.com>
2014-11-21 23:20:18 +0300

    clean up project for release

--- android-build-release.sh
+++ android-build-release.sh
@@ -2,8 +2,8 @@ jarsigner="C:\Program Files (x86)\Java\jdk1.7.0_55\bin\jarsigner"
 keytool="C:\Program Files (x86)\Java\jdk1.7.0_55\bin\keytool"
 zipalign="C:\Users\max\AppData\Local\Android\android-sdk\build-tools\20.0.0\zipalign"
 
+NODE_ENV=cordova_stage gulp build
 ionic build android --release
-"$keytool" -genkey -v -keystore platforms/android/ant-build/my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
-"$jarsigner" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore platforms/android/ant-build/my-release-key.keystore platforms/android/ant-build/SurfBetter-release-unsigned.apk alias_name
-rm .release/local-built/android/SurfBetter.apk
-"$zipalign" -v 4 platforms/android/ant-build/SurfBetter-release-unsigned.apk .release/local-built/android/SurfBetter.apk
+"$jarsigner" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore platforms/android/ant-build/my-release-key.keystore platforms/android/ant-build/RideBetter-release-unsigned.apk alias_name
+rm .release/local-built/android/RideBetter.apk
+"$zipalign" -v 4 platforms/android/ant-build/RideBetter-release-unsigned.apk .release/local-built/android/RideBetter.apk
